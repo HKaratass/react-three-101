63006b23352c548ae424742bb95aa2ea5c36a2e6
HKaratass <132591980+HKaratass@users.noreply.github.com>
2024-10-16 13:25:44 +0300

    Update Base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-three-101-store/",
+  base: "/react-three-101/",
 })
